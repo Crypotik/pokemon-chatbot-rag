85738a9d64a9987751a4a591173d34ab37a899ce
Crypotik <pulsifer.andrew@gmail.com>
2026-08-13 20:57:16 -0400

    Added another data source, still need to find more data for this to be useful.

--- src/ingest.py
+++ src/ingest.py
@@ -3,27 +3,37 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 # load data/docs
-filepath = "/home/crypotik/learning_rag/pokemon_chatbot/data/poke_corpus.csv"
-loader = CSVLoader(file_path=filepath)
+filepath = os.getenv("DATAFILE_PATH")
+loader = CSVLoader(
+    file_path=filepath,
+    metadata_columns=["Total","HP","Attack","Defense","Sp. Atk","Sp. Def","Speed","Generation"],
+    content_columns=["Name","Form","Type1","Type2"],
+    source_column="ID"
+    )
 data = loader.load()
 
-# split data
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=64)
-chunks = text_splitter.split_documents(data)
+# split data (only when CSV rows are above chunk_size)
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=64)
+# chunks = text_splitter.split_documents(data)
 
 # add nomic prefix to chunks
-for chunk in chunks:
+for chunk in data:
     chunk.page_content = f"search document: {chunk.page_content}"
 
 # init Nomic
 embeddings = OllamaEmbeddings(model="nomic-embed-text")
 
 # create/save to vector db
-vectordb_filepath = "/home/crypotik/learning_rag/pokemon_chatbot/chroma"
+vectordb_filepath = os.getenv("CHROMA_DATABASE")
 vector_store = Chroma.from_documents(
-    documents=chunks, 
-    embedding=embeddings, 
+    documents=data, 
+    embedding=embeddings,
     persist_directory=vectordb_filepath)
 
 print(f"successfully ingested data to {vectordb_filepath}")
--- src/query.py
+++ src/query.py
@@ -4,6 +4,10 @@ from langchain_community.llms import Ollama
 from langchain_classic.chains import create_retrieval_chain
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # 1. Direct the embedder to use the exact same Nomic settings
 embeddings = OllamaEmbeddings(
@@ -12,17 +16,27 @@ embeddings = OllamaEmbeddings(
 )
 
 # 2. LOAD the existing, pre-computed database from disk
-vectordb_filepath = "/home/crypotik/learning_rag/pokemon_chatbot/chroma"
+vectordb_filepath = os.getenv("CHROMA_DATABASE")
 vector_store = Chroma(
     persist_directory=vectordb_filepath, 
     embedding_function=embeddings
 )
-retriever = vector_store.as_retriever(search_kwargs={"k": 3})
+retriever = vector_store.as_retriever(search_kwargs={"k": 5})
 
-# 3. Set up your local Llama 3.1 1B generator
+# 3. Set up your local Llama 3.1 8B generator
 llm = Ollama(model="llama3.1:8b", temperature=0.2)
 
-system_prompt = "Answer the user question using only this context:\n\n{context}"
+system_prompt = """
+You are a helpful assistent that specializes in Pokemon, answer the user's question using only the context provided.
+
+Refer to the chat history ONLY when you can not understand what the user is asking.
+DO NOT TREAT THE CHAT HISTORY AS FACTUAL TRUTH WHEN ANSWERING THE USER.
+
+If the answer cannot be found in the context, answer with I don't know.
+
+Context:
+{context}
+"""
 prompt = ChatPromptTemplate.from_messages([
     ("system", system_prompt),
     ("human", "{input}"),
@@ -30,11 +44,29 @@ prompt = ChatPromptTemplate.from_messages([
 
 rag_chain = create_retrieval_chain(retriever, create_stuff_documents_chain(llm, prompt))
 
+chat_history = []
+
 # 4. Interactive user loop
 while True:
     user_input = input("\nAsk your data a question (or type 'exit'): ")
     if user_input.lower() == 'exit':
         break
         
-    response = rag_chain.invoke({"input": user_input})
-    print(f"\nAI: {response['answer']}")
+    history_text = "\n".join(
+        f"User: {user}\nAI: {assistant}"
+        for user, assistant in chat_history
+    )
+
+    response = rag_chain.invoke({
+        "input": user_input,
+        "chat_history": history_text
+    })
+
+    answer = response["answer"]
+
+    print(f"\nAI: {answer}")
+
+    chat_history.append((user_input, answer))
+
+    if len(chat_history) > 8:
+        chat_history = chat_history[-8:]
